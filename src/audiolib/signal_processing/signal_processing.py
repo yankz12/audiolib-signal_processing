d21a61d6dca13fc6e2fdf89fa7ba47e4a71b7937
yankz12 <sa.may@posteo.net>
2024-02-05 18:52:01 +0100

    Fix frequency vector creation via np.fft.rfftfreq

--- src/audiolib/signal_processing/signal_processing.py
+++ src/audiolib/signal_processing/signal_processing.py
@@ -6,7 +6,7 @@ from scipy.fftpack import fftshift
 def get_rfft_power_spec(x, fs, Nfft=None):
     if Nfft is None:
         Nfft = len(x)
-    freq = np.arange(Nfft/2+1)/(Nfft/2+1)*fs/2
+    freq = np.fft.rfftfreq(len(x), 1/fs)
     Sxx = np.abs(np.fft.rfft(x, Nfft) / Nfft)**2
     return freq, Sxx
 
