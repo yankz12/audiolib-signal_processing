812a15df755b4935376b25a0c997975d14d6785c
yankz12 <sa.may@posteo.net>
2025-02-21 10:31:11 +0100

    Init sweep by novak

--- src/audiolib/signal_processing/signal_processing.py
+++ src/audiolib/signal_processing/signal_processing.py
@@ -2,6 +2,65 @@ import numpy as np
 import scipy.signal as scsp
 
 from scipy.fftpack import fftshift
+from typing import Optional
+
+class NovakSweep():
+    f1 : float
+    f2: float
+    fs : int
+    approx_dur: float
+    apply_fade : bool
+    dur_fade_in : Optional[float]
+    dur_fade_out : Optional[float]
+
+    def __post_init__(self, ):
+        if self.apply_fade:
+            self._samples_fade_in  = int(self.fade_len_in*self.fs)
+            self._samples_fade_out = int(self.fade_len_out*self.fs)
+        else:
+            self._samples_fade_in  = None 
+            self._samples_fade_out = None
+
+        self._L = self.approx_dur/np.log(self.f2/self.f1)
+
+    def get_sweep_signal(self, ):
+        t = np.arange(0,np.round(self.fs*self.approx_dur-1)/self.fs,1/self.fs)  # time axis
+        s = np.sin(2*np.pi*self.f1*self._L*np.exp(t/self._L))       # generated swept-sine signal
+
+        if self.apply_fade:
+            s = self._apply_fade_in_out(s, 'both')
+
+        return t, s,
+
+    def _apply_fade_in_out(self, sweep, where, ):
+        """
+        Parameters
+        ----------
+        sweep : np.ndarray
+            sweep signal to be faded in and/or out
+        where : str, one out of ['in', 'out', 'both']
+            Apply only fade-in, only fade-out or both
+        """
+        if where == 'in' or where == 'both':
+            sweep[0:self._samples_fade_in] = sweep[0:self._samples_fade_in] * (
+                (
+                    -np.cos(np.arange(self._samples_fade_in)
+                    / self._samples_fade_in*np.pi)+1
+                ) / 2
+            )
+        if where == 'out' or where == 'both':
+            sweep[-self._samples_fade_out:] = sweep[-self._samples_fade_out:] *  (
+                (
+                    np.cos(np.arange(self._samples_fade_out)
+                    / self._samples_fade_out*np.pi)+1
+                ) / 2
+            )
+        return sweep
+
+
+
+
+
 
 def get_rfft_spec(x, fs, Nfft=None):
     if Nfft is None:
