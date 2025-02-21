cf8d6fc72f33c1c88c47afed69518cc6f9bcf7e5
yankz12 <sa.may@posteo.net>
2025-02-21 15:44:15 +0100

    Fix incorrect phase of swept signal

--- test/test_exp_sweep.py
+++ test/test_exp_sweep.py
@@ -7,37 +7,53 @@ from audiolib.signal_processing import ExpSweep
 
 plt.close('all')
 
+# ----------------------------------------------------------------------------
+# Sweep definitions
 fs = 48000
 f1 = 2e3
 f2 = 20e3
 dur = 5
 apply_fade_to = 'both'
+len_irs = 2**12
+n_harms = 1
 
+# ----------------------------------------------------------------------------
+# Linear test filter definition 
+fir_len = 100
+fir_cutoff = fs/4
+fir = sc_sp.firwin(numtaps=100, cutoff=fs/4, fs=fs,)
+
+# ----------------------------------------------------------------------------
+# Sweep creation
 sweep = ExpSweep(
     fs=fs,
     f1=f1,
     f2=f2,
     approx_dur = dur,
 )
-
 t_sweep, s_sweep = sweep.get_sweep_signal()
 
-fir = [.5, .5] # FIR Low-Pass with cutoff @ fs/2
-
-fir = sc_sp.firwin(numtaps=100, cutoff=fs/4, fs=fs,)
-
+# ----------------------------------------------------------------------------
+# Convolution of sweep and linear test filter
 y = sc_sp.lfilter(b = fir, a = [1, 0], x = s_sweep, )
 
+# ----------------------------------------------------------------------------
+# Extraction of higher harmonic frequency functions
 t_hs, hs, freq_Hs, Hs = sweep.get_hhfrfs(
      y=y,
-     n_harms=2,
+     n_harms=n_harms,
+     len_irs = len_irs
 )
+Hs_correct_phase = [Hs_tmp*np.exp(-1j*freq_Hs*np.pi*len_irs/fs) for Hs_tmp in Hs]
+Hs_correct_phase = np.array(Hs_correct_phase)
 
-fig, ax = al_plt.plot_rfft_freq(
-    f=freq_Hs,
-    data = 20*np.log10(abs(Hs.transpose())),
+# ----------------------------------------------------------------------------
+# Plotting higher harmonic frequency functions
+fig, ax_mag, ax_arg = al_plt.plot_mag_phase(
+    freq_h=freq_Hs,
+    magnitude = 20*np.log10(abs(Hs.transpose())),
+    phase_deg = np.unwrap(np.angle(Hs_correct_phase.transpose()))/np.pi*180,
 )
-ax.legend(('1st harmonic','2nd harmonic'), loc=3)
-ax.set_xlim(0, fs/2)
+ax_mag.legend(('1st harmonic','2nd harmonic'), loc=3)
+ax_mag.set_xlim(0, fs/2)
 plt.show(block=False)
-
