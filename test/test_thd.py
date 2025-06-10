401157372e4ec562ad51f2e22075136bb34d66ce
AdrianS <adrian.may@sonova.com>
2025-06-10 08:27:23 +0200

    Adjust test routine for THD Calculator

--- test/test_thd.py
+++ test/test_thd.py
@@ -9,7 +9,7 @@ t = np.linspace(0, dur, int(dur*fs))
 f0 = 103.2
 num_harms = 4
 
-x = np.sin(2*np.pi*f0*t)
+x = 2*np.sin(2*np.pi*f0*t)
 x += 0.1*np.sin(2*np.pi*2*f0*t)
 x += 0.05*np.sin(2*np.pi*3*f0*t)
 x += 0.05*np.sin(2*np.pi*4*f0*t)
@@ -19,10 +19,10 @@ thd = al_sc.thd_from_time_sig(
     x,
     fs,
     num_harms = num_harms,
-    tol_hz = 10,
+    tol_hz = 0.5,
     plot_spec = True,
 )
-thd_by_hand = 100*np.sqrt(0.1**2 + 0.05**2 + 0.05**2 + 0.05**2)
+thd_by_hand = 100*(np.sqrt(0.1**2 + 0.05**2 + 0.05**2 + 0.05**2) / 2 )
 
 print(f'THD by Repo: {np.round(thd, 2)} %')
 print(f'THD by Hand: {np.round(thd_by_hand, 2)} %')
